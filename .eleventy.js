a4fd35404b5f127b26a2ea9ee6b61b4cb66ecf16
hj_macbook <rjcnd123@gmail.com>
2021-02-19 21:03:32 +0900

    success build

--- .eleventy.js
+++ .eleventy.js
@@ -4,16 +4,16 @@ const esbuild = require("esbuild")
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
 
-  eleventyConfig.addWatchTarget("./_tmp/style.css");
-  eleventyConfig.addPassthroughCopy({'./_tmp/style.css': './style.css'});
+  if(!process.env.ELEVENTY_PRODUCTION){
+    eleventyConfig.addWatchTarget("./_tmp/style.css");
+    eleventyConfig.addPassthroughCopy({'./_tmp/style.css': './style.css'});
 
-  eleventyConfig.addWatchTarget("./_tmp/index.js");
-  eleventyConfig.addPassthroughCopy({'./_tmp/index.js': './index.js'});
-
+    eleventyConfig.addWatchTarget("./_tmp/index.js");
+    eleventyConfig.addPassthroughCopy({'./_tmp/index.js': './index.js'});
+  }
   eleventyConfig.addPassthroughCopy({
     "./node_modules/alpinejs/dist/alpine.js": "./alpine.js",
   });
-
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (
@@ -31,6 +31,7 @@ module.exports = function (eleventyConfig) {
 
     return content;
   });
+
   return {
     dir: {
       input: 'src',
